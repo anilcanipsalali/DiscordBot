8d502eaf13460bfd1f516199e9b7d7aee585c393
Anılcan İpsalalı <anilcan.ipsalali@gmail.com>
2020-12-04 10:59:41 +0000

    Nothing changed.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const replies = ["Konuşamam", "Yürü git", "Benimle uğraşma", "Seni sevmiyor
 client.on("ready", ready => {
   console.log("Bot hazır!");
 
-  client.user.setActivity("discord.gg/dRTjqU7 | ~help");
+  client.user.setActivity("Trouble5 | ~help");
 });
 
 client.on("message", message => {
@@ -34,9 +34,14 @@ client.on("message", message => {
 			})
 		}else
 
+		if(message.content === prefix + 'discordlink')
+		{
+			message.channel.send('discord.gg/dRTjqU7')
+		}else
+
 		if(message.content === prefix + 'help')
 		{
-			message.channel.send(`**~konus** - Saçma sapan bir bot komutu :laughing:.\n**~ping** - Server'daki pingi gösterir.\n**~severbilgi** - Server'daki kişi sayısını gösterir.\n**~temizle <mesaj sayısı>** - Kanaldaki mesajları siler.(Henüz yapım aşamasında..:timer:)`)
+			message.channel.send(`**~konus** - Saçmasapan bir bot komutu :laughing:.\n**~ping** - Server'daki pingi gösterir.\n**~severbilgi** - Server'daki kişi sayısını gösterir.\n**~temizle <mesaj sayısı>** - Kanaldaki mesajları siler.(Henüz yapım aşamasında..:timer:)`)
 		}else
 
 		if(message.content === prefix + 'temizle' && message.member.hasPermission('Admins'))
@@ -48,13 +53,10 @@ client.on("message", message => {
 			else if(numberofmessages > 100) return message.reply(`100\'den fazla mesaj silemezsiniz.`)
 
 			else if(numberofmessages < 1) return message.reply(`En az 1 mesaj silebilirsiniz.`)
-			
 		}
-
+		
 	}else return;
 });
 
-//Functions
-
-
+//Login
 client.login(process.env.BOTTOKEN);
